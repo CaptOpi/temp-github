c9315403530f14016de384e2d8d2423cf06a0637
Ahmed <82651@gehrhsd.net>
2022-09-21 16:18:59 -0400

    Test

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
 'use strict';
 
 const className = document.body.className
-;
-const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    if(className = document.body.className) {
-        this.textContent = "Dark";
-    } else {
-        this.textContent = "Light";
-    }
-});
